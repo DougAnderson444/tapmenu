13cf195021e0940f14486b6f865937df91186c04
Doug A <douganderson444@gmail.com>
2023-02-17 16:02:29 -0400

    vanilla route

---
+++ src/routes/custom/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let target;
+	onMount(async () => {
+		const TapMenu = (await import('../../../dist/esm/TapMenu.svelte.js')).default;
+
+		new TapMenu({
+			target: document.body,
+			props: {
+				content: '<p>Hello <b>World</b><p>'
+			}
+		});
+	});
+</script>
+
+<div id="target" />
