8103ec3a96b5365b075ddc4ddaf4a7464a90d115
Doug A <douganderson444@gmail.com>
2023-02-17 16:01:31 -0400

    switch to svgs

--- src/lib/icons/TextSize.svelte
+++ src/lib/icons/TextSize.svelte
@@ -1,6 +1,3 @@
-<script>
-	import Icon from '@iconify/svelte';
-	import formatTextSize from '@iconify/icons-zondicons/format-text-size';
-</script>
-
-<Icon icon={formatTextSize} />
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"
+	><path fill="currentColor" d="M16 9v8h-2V9h-4V7h10v2h-4zM8 5v12H6V5H0V3h15v2H8z" /></svg
+>
