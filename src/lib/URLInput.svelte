d0cec84d9f6f7b109a9de64b32b7efe4834ef3f9
Doug A <douganderson444@gmail.com>
2023-03-29 09:47:57 -0300

    add url input

---
+++ src/lib/URLInput.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	export let previousUrl = 'https://';
+	let value = previousUrl;
+	const clicked = (e) => dispatch('value', value);
+</script>
+
+<div class="bg-neutral-800 text-white rounded-md shadow-md px-4 py-2">
+	<form>
+		<input type="text" bind:value class="rounded p-1 text-black" /><button
+			on:click={clicked}
+			class="m-1">Go</button
+		>
+	</form>
+</div>
