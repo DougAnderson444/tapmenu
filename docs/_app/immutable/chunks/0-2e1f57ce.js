7943345fbf2148e7156dcf1b259f4195b1bec2cc
Doug A <douganderson444@gmail.com>
2023-03-29 19:31:20 -0300

    updated gh pages build

--- docs/_app/immutable/chunks/0-2e1f57ce.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-36815f85.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-8c1aa7c6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-88ce7b77.js";export{t as component};
