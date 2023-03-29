6b8792bc398a4d7d356b1cfbb05f74aed0a7becc
Doug A <douganderson444@gmail.com>
2023-03-29 19:28:23 -0300

    cache data in browser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,44 @@
 <script>
+	import { onMount } from 'svelte';
 	import { TapMenu } from '$lib';
 	import demo from './_demo.html?raw';
+
+	const LOCAL_STORAGE_KEY = 'content';
 	let content = '';
+	let handleContentUpdate = (e) => (content = e.detail);
+
+	onMount(() => {
+		handleContentUpdate = (e) => {
+			// update content display, AND
+			content = e.detail;
+			//save to localstorage
+			localStorage.setItem(LOCAL_STORAGE_KEY, e.detail);
+		};
+
+		// load if localStorage "content"
+		const maybeContent = localStorage.getItem(LOCAL_STORAGE_KEY);
+		if (maybeContent) {
+			content = maybeContent;
+			console.log('Context set to ', { content });
+		}
+	});
 </script>
 
 <div class="p-8">
 	<div class="border rounded p-2 my-6">
-		<h1>TapMenu - Svelte/Tailwind Context Menu</h1>
+		<h1>TapMenu + Svelte + Tailwind</h1>
 		<div class="bg-neutral-200 p-2">
-			<TapMenu on:update={(e) => (content = e.detail)}>
-				{@html demo}
-				<!-- <p>Hello World! 🌍️</p>
+			{#if content}
+				<TapMenu on:update={handleContentUpdate} {content} />
+			{:else}
+				<TapMenu on:update={handleContentUpdate}>
+					{@html demo}
+					<!-- <p>Hello World! 🌍️</p>
 				<p>
 					<span class="bg-yellow-300"> Highlight/Select</span> to bring up bubble menu.
 				</p>-->
-			</TapMenu>
+				</TapMenu>
+			{/if}
 		</div>
 	</div>
 
