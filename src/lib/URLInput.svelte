26f9a3796d081adc2d8ddecec7e0cc3b5af38871
Doug Anderson444 <douganderson444@gmail.com>
2024-04-09 19:39:09 -0300

    autofocus and flex URLs

--- src/lib/URLInput.svelte
+++ src/lib/URLInput.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div class="bg-neutral-800 text-white rounded-md shadow-md px-4 py-2">
-	<form>
-		<input type="text" bind:value class="rounded p-1 text-black" /><button
+	<form class="flex">
+		<input type="text" bind:value class="rounded p-1 text-black" autofocus /><button
 			on:click={clicked}
 			class="m-1">Go</button
 		>
